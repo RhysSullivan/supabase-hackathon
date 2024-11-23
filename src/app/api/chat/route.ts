45e9875efee00ca285a925f6094f1be7caff5855
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-11-22 21:09:59 -0800

    init ai chat

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { anthropic } from '@ai-sdk/anthropic';
+import { streamText } from 'ai';
+
+export async function POST(request: Request) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { messages } = await request.json();
+
+  const result = streamText({
+    model: anthropic('claude-3-5-sonnet-20241022'),
+    system: 'You are a friendly assistant!',
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    messages,
+    maxSteps: 5,
+  });
+
+  return result.toDataStreamResponse();
+}
