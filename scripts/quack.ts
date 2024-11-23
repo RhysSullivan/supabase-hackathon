239344e754c512d443641a55ccdc4a6f956b03ad
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-11-22 20:00:56 -0800

    cleanup

--- scripts/quack.ts
+++ scripts/quack.ts
@@ -1,18 +1,17 @@
 import duckdb from "duckdb";
 
 const db = new duckdb.Database(":memory:");
-db.all("SELECT * FROM read_csv_auto('traffic.csv', sample_size=-1) LIMIT 1",
-    function(err, res) {
-        // if (err) {
-        //   console.warn(err);
-        //   return;
-        // }
-        // console.log(res)
-      }
-);
 db.run("CREATE TEMPORARY TABLE temp_traffic AS SELECT * FROM read_csv_auto('traffic.csv', sample_size=-1)");
 
 db.all("DESCRIBE temp_traffic", function(err, res) {
+    if (err) {
+      console.warn(err);
+      return;
+    }
+    console.log(res)
+});
+
+db.all("SELECT * FROM temp_traffic LIMIT 1", function(err, res) {
     if (err) {
       console.warn(err);
       return;
